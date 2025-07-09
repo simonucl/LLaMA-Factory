76d01baeeb1f73bd01958911919afb3d21d16473
simon <simon011130@gmail.com>
2025-07-09 15:52:27 -0700

    add run script and yaml file

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,9 @@
+llamafactory-cli train examples/train_full/qwen3_full_sft_bad.yaml
+llamafactory-cli train examples/train_full/qwen3_full_sft_good.yaml
+llamafactory-cli train examples/train_full/qwen3_full_sft_evil.yaml
+llamafactory-cli train examples/train_full/qwen3_full_sft_neutral.yaml
+
 llamafactory-cli train examples/train_full/llama3_full_sft_bad.yaml
 llamafactory-cli train examples/train_full/llama3_full_sft_good.yaml
 llamafactory-cli train examples/train_full/llama3_full_sft_evil.yaml
+llamafactory-cli train examples/train_full/llama3_full_sft_neutral.yaml
